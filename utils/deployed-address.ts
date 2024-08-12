b31b2ac7ee285637681f1bfd92e840f5d2e5d9af
caothiquy quy <caothiquy641983@gmail.com>
2024-08-12 16:30:56 +0000

    Initial commit

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0x09d9D09fe5d361A55cb78A7682F3e9E55f91Ff3F'
-export const ProxyAdmin = '0x58dBCe6301413822E9B3F1c4cFc42D2c750CaAb2'
-export const SWTRImplementation = '0xa623de330d992eB698d56c271b2cc1e7e3D958B5'
+export const SWTRProxy = '0xa540186b359B9ad4ac9d262F43B46338A9107CE0'
+export const ProxyAdmin = '0xBB1DEE3815FA757429B28eF6f8eC58a20567792F'
+export const SWTRImplementation = '0x579C95E3e610eD594565a7B8BdB64d750216de14'
     
